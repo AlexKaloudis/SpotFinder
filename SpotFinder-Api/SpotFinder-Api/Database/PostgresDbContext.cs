91987fbf0551e9eb9354152d27db830ab09668ee
Alexandros Kaloudis <a.kaloudis@msensis.com>
2024-03-05 22:50:09 +0200

    Postgres db connection was added. Docker-compose file was added.

---
+++ SpotFinder-Api/SpotFinder-Api/Database/PostgresDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpotFinder_Api.Models;
+using System;
+
+namespace SpotFinder_Api.Database
+{
+    public class PostgresDbContext : DbContext
+    {
+        public PostgresDbContext(DbContextOptions<PostgresDbContext> options) : base(options) { }
+
+        public DbSet<Spot> spots {  get; set; } 
+    }
+}
---
+++ SpotFinder-Api/SpotFinder-Api/Migrations/20240305180421_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SpotFinder_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "spots",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Link = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<string>(type: "text", nullable: false),
+                    Paid = table.Column<string>(type: "text", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: false),
+                    Image = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_spots", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "spots");
+        }
+    }
+}
